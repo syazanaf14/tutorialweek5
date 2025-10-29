84415012a474b07530f2271fd0d56510fd82f9a4
syazanaf14 <s23b0125@siswa.umk.edu.my>
2025-10-29 18:12:50 +0800

    Update ga.py

--- ga.py
+++ ga.py
@@ -7,7 +7,7 @@ POP_SIZE = 500
 MUT_RATE = 0.2
 
 #TARGET: Our goal.
-TARGET = 'fakhitah'
+TARGET = 'fatin'
 
 #GENES: Options from which our population would be created.
 GENES = ' abcdefghijklmnopqrstuvwxyz'
