1cb073cafef1a09f17fc374140b2186393e6de0d
syazanaf14 <s23b0125@siswa.umk.edu.my>
2025-10-29 18:19:21 +0800

    Update ga.py

--- ga.py
+++ ga.py
@@ -117,10 +117,10 @@ def main(POP_SIZE, MUT_RATE, TARGET, GENES):
       population = replace(new_gen, population)
 
       if (population[0][1] == 0):
-        print('Target found')
-        print('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
+        st.write('Target found')
+        st.write('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
         break
-      print('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
+      st.write('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
       generation+=1
 
 result = main(POP_SIZE, MUT_RATE, TARGET, GENES)
